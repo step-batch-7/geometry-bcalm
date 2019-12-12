9127d533b038b5d8dfe310950a9f0c207872ceb4
Vikram <58028680+bcalm@users.noreply.github.com>
2019-12-12 16:22:24 +0530

    time up. Forced commit

--- src/line.js
+++ src/line.js
@@ -6,11 +6,17 @@ class Line {
     this.y2 = secondPosition[1];
   }
 
-  createString() {
-    return `Line start from ${this.x1},${this.y1} and ends with ${this.x2},${this.y2}`;
+  toString() {
+    return `Line start from (${this.x1},${this.y1}) and ends with (${this.x2},${this.y2})`;
   }
-  get toString() {
-    return this.createString();
+
+  isEqualTo(anotherLine) {
+    return (
+      this.x1 == anotherLine.x1 &&
+      this.x2 == anotherLine.x2 &&
+      this.y1 == anotherLine.y1 &&
+      this.y2 == anotherLine.y2
+    );
   }
 }
 
--- test/lineTest.js
+++ test/lineTest.js
@@ -5,9 +5,15 @@ describe("Line", () => {
   describe("toString", () => {
     it("should convert given data to string", () => {
       const a = new Line([1, 2], [2, 3]);
-      const actual = a.toString;
-      const expected = "Line start from 1,2 and ends with 2,3";
+      const actual = a.toString();
+      const expected = "Line start from (1,2) and ends with (2,3)";
       assert.deepStrictEqual(actual, expected);
+    });
+  });
+  describe("isEqualTo", () => {
+    it("should give true if given line is equal to our line", () => {
+      const anotherLine = { x1: 1, y1: 2, x2: 3, y2: 4 };
+      assert.ok(Line.isEqualTo(anotherLine));
     });
   });
 });
